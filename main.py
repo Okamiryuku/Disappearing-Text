ed95a31dfcdbe407473a180ba3d8496d613f9d92
Okamiryuku <okamiryuku@gmail.com>
2023-11-09 20:50:21 -0600

    Format and final editing

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from random import choice
+
 
 app = Flask(__name__)
+
+with open('static/phrases.txt', 'r') as file:
+    lines = file.readlines()
+
+lines = [line.strip() for line in lines]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    phrases = choice(lines)
+    return render_template('index.html', phrases=phrases)
 
 
 if __name__ == '__main__':
